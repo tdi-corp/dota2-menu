61ccfc166415dc57f9498663a304a4a5467b0366
tdi-github <d1mmas28@gmail.com>
2024-09-12 22:23:05 +0300

    express 4 versel

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 import express from "express";
 import path from "path"
-// const express = require('express')
-// const path = require('path')
 
 const __dirname = import.meta.dirname;
 const app = express()
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'src/index.html'))
+    res.sendFile(path.join(__dirname, 'public/index.html'))
 })
 
 app.use('/assets', express.static(path.join(__dirname, 'public')))
